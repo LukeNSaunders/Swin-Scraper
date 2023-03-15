34bcd713fc8436cfc790de739ecbc50acd152775
Luke Saunders <lukensaunders@gmail.com>
2023-03-15 18:30:32 +0000

    add return types response

--- server/src/controllers/scrapeOddsByEvent.ts
+++ server/src/controllers/scrapeOddsByEvent.ts
@@ -3,18 +3,16 @@ import { Request, Response } from 'express';
 
 export const scrapeOddsByEvent = async (req: Request, res: Response) => {
 
-  const { eventURL } = req.body;
-
-  if (!eventURL) {
-    return res.status(400).send('Please provide valid URL');
-  }
-  
   try {
+    const { eventURL } = req.body;
+    if (!eventURL) {
+      return res.status(400).send('Please provide valid URL');
+    }
     const odds = await scrapeHorseInfo(eventURL);
     res.status(201);
-    res.send(odds);
+    res.json(odds);
   } catch (error) {
     res.status(500);
-    console.log('ERROR', error);
+    console.log('ERROR!', error);
   }
 };
---
+++ server/src/returnTypes.d.ts
@@ -0,0 +1,4 @@
+export interface horseInfoResponse {
+  horseName: string; 
+  horseOdds: string; 
+}
