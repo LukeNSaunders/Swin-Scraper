e316c58331905a47a57da6aeacdf4d0d99f4994f
Luke Saunders <lukensaunders@gmail.com>
2023-03-16 02:10:08 +0000

    register user extra checks, functioning

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -2,28 +2,28 @@ import User, {UserDocument} from "../models/userSchema";
 import bcrypt from 'bcrypt'
 import { Request, Response } from "express";
 
-const saltRounds : number = 12 
-
 export const registerUser = async (req: Request,res: Response) : Promise<void>=> {
   const {username, email, password} = req.body
 
   try {
-    const checkUsername  = await User.findOne({username:username})
-    const checkEmail = await User.findOne({email:email})
-    if (checkUsername || checkEmail) {
+    // check for already existing user 
+    const usernameExists = await User.findOne({username:username})
+    const emailExists = await User.findOne({email:email})
+
+    if (usernameExists|| emailExists) {
       res.status(409).send({message:"Username already exists", status:409})
       return; 
     }
     if (password === "") {
       res.status(400).send({message:"Password cannot be empty", status:400})
     }
-
+    // hash and store password if user does not exist 
+    const saltRounds : number | string = await bcrypt.genSalt()
     const hashedPassword : string = await bcrypt.hash(password, saltRounds)
-    const newUser : UserDocument = new User({...req.body, password : hashedPassword})
-    const savedUser : UserDocument = await newUser.save();
-    
-    res.status(201).send(savedUser)
-  
+
+    //create new user 
+    const newUser : UserDocument = await User.create({...req.body, password : hashedPassword})
+    res.status(201).send(newUser)
   } catch (error) {
     console.log('ERROR', error)
   }
